a7a92d8115389c57e490f2d5fe4f50b760e22d19
Viraj H <virajh@ubuntu.(none)>
2013-11-27 12:18:22 -0800

    First push, sample code added.

---
+++ simpleExample/client.py
@@ -0,0 +1,60 @@
+import httplib2
+import urllib
+from json import loads, dumps
+
+class sampleClient:
+
+	def __init__(self, api_url):
+		global url
+		url = api_url
+		global h
+	        h = httplib2.Http(".cache")  
+
+	def getUsers(self):
+		response, body = h.request(url+"users", "GET", headers=None)
+		print response
+		return body
+
+	def getUser(self, name, head):
+		response, body = h.request(url+"users/%s" % (name), "GET", headers=head)
+		return body
+
+	def create(self, name, body, head):
+		response, body = h.request(url+"users/%s" % (name), "POST", headers=head, body=body)
+		return body
+
+	def modifyName(self, name, newname, head):
+		response, body = h.request(url+"users/%s/%s" %(name, newname), "PUT", headers=head)
+		return body
+
+	def modify(self, name, body, head):
+		response, body = h.request(url+"users/modify/%s" %(name), "PUT", headers=head, body=body)
+		return body
+
+	def delete(self, name, head):
+		response, body = h.request(url+"users/%s" %(name), "DELETE", headers=head)
+		return body
+
+	def greet(self, head):
+		response, body = h.request(url+"hello", "GET", headers=head)
+		return body
+
+	def testJson(self, data, header):
+		response, body = h.request(url+"test", "GET", headers=header, body=data)
+		return body
+
+client = sampleClient('http://localhost:8080/')
+headers = {'content-type': 'application/json'}
+data = {"age": "dead", "location": "unknown"}
+body = dumps(data)
+
+#print client.greet()
+#print client.testJson(body, headers)
+#print client.create("jack", body, headers)
+#print client.create("jack", body, headers)
+#print client.getUsers()
+#print client.getUser('viraj', headers)
+#print client.modify("viraj", body, headers)
+#print client.modifyName('jack', 'Jack', headers)
+#print client.delete('jack', headers)
+
---
+++ simpleExample/server.py
@@ -0,0 +1,91 @@
+from bottle import *
+import json
+
+people = { "viraj": {"age": 23, "country": "india"}, "jeff": {"age": 67, "country": "finland"}}
+#GET method samples
+@route('/hello', method='GET')
+def hello():
+    return "Hello World!"
+
+@route('/users', method='GET')
+def getUsers():
+    return BaseResponse(body=json.dumps(people), status=200)
+
+@route('/users/<name>', method='GET')
+def getUser(name=None):
+    if(name):
+	if name in people:
+		return json.dumps(people[name])
+	else:
+		abort(404, "%s not found" % (name))
+    else:
+	abort(400, "Bad Request: Missing Argument")
+
+#DELETE method sample
+@route('/users/<name>', method='DELETE')
+def deleteUser(name=None):
+	if(name):
+		if name in people:
+			people.pop(name)
+			return "Success: Deleted %s" % (name)
+		else:
+			abort(404, "%s not found" % (name))
+	else:
+		abort(400, "Bad Request: Missing Argument")
+
+#PUT method samples
+@route('/users/modify/<old>', method='PUT')
+def editUserInfo(old=None):
+	print "InHere"
+	if not old:
+		abort(400, "Bad Request: Missing Argument")
+	if old in people:
+		body = request.body.read()
+		data = json.loads(body)
+		people[old] = data
+		return "Success: Modified %s" % (old)
+	else:
+		abort(404, "%s not found" % (name))
+
+@route('/users/<old>/<new>', method='PUT')
+def editUserName(old=None, new=None):
+	print "orHere"
+	if not old:
+		abort(400, "Bad Request: Missing Argument")
+	if not new:
+		abort(400, "Bad Request: Missing Argument")
+	if new in people:
+		abort(409, "Conflict: %s already exists" %(new))
+	if old in people:
+		people[new]=people[old]
+		people.pop(old)
+		return "Success: Modified %s to %s" % (old, new)
+	else:
+		abort(404, "%s not found" % (old))
+
+
+#POST method sample
+@route('/users/<name>', method='POST')
+def addUser(name=None):
+	if name:
+		if name in people:
+			abort(409, "Conflict: %s already exists" %(new))
+		else:
+			body = request.body.read()
+			data = json.loads(body)
+			people[name] = data
+			return "Success: User %s added" % (name)
+	else:
+		abort(400, "Bad Request: Missing Argument")
+
+#JSON tester method
+@route('/test', method='GET')
+def call():
+	print request
+	body = request.body.read()
+	print type(body)
+	data = json.loads(body)
+	print type(data)
+	return "Success"
+
+run(host='localhost', port=8080, debug=True)
