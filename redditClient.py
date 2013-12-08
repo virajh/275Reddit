0c5ed62b30dd1350ca104c6a4afa33c250ad16f3
Viraj H <virajh@ubuntu.(none)>
2013-12-07 22:30:32 -0800

    All functions working.

--- redditClient.py
+++ redditClient.py
@@ -22,7 +22,31 @@ class redditClient:
 	def create_topic(self, data):
 		#the method to create topics
 		response, body = httpConn.request(url+"topics", "POST", headers=head, body=data)
-		return json.loads(body)
+		return body
+
+
+	def get_topic(self, topicId):
+		#the method to find a topic given its ID
+		response, body = httpConn.request(url+"topics/%s" %(topicId), "GET", headers=head)
+		return body
+
+
+	def delete_topic(self, topicId):
+		#the method to delete a topic given its ID
+		response, body = httpConn.request(url+"topics/%s" %(topicId), "DELETE", headers=head)
+		return response.status
+
+
+	def post_comment(self, topicId, data):
+		#method to post comments
+		response, body = httpConn.request(url+"topics/%s/comments" %(topicId), "POST", headers=head, body=data)
+		return response.status
+
+
+	def get_all_topics(self):
+		#method to query all topics
+		response,body = httpConn.request(url+"topics", "GET", headers=head)
+		return body
 
 
 #The code above is OO code with a class defined for the reddit client.
@@ -31,12 +55,20 @@ class redditClient:
 
 #create client for reddit server
 client = redditClient("http://localhost:8080/")
-
 #sample data
-topic = {"topicName": "sample", "author": "viraj", "desc": "this is a test topic"}
+topic = {"topicName": "comment1", "author": "viraj", "desc": "this is a comment test topic"}
 #jsonified data
 data = json.dumps(topic)
 
 #response of the create_topic call
 response = client.create_topic(data)
-print response['topicId']
+#print response
+tid = json.loads(response)['topicId']
+
+data = {"author": "viraj", "comment": "commented by viraj"}
+
+print client.post_comment(topicId=tid, data=json.dumps(data))
+print client.delete_topic(tid)
+print client.get_topic(tid)
+#print client.get_all_topics()
+
--- redditServer.py
+++ redditServer.py
@@ -5,16 +5,21 @@ import json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+
 #Connection to MongoDB
 mongo = MongoClient('localhost', 27017)
+
 #Connect to database called reddit
 db = mongo['reddit']
+
 #connect to topics collection in the reddit db
 global topics
 topics = db['topics']
+
 #http header object to be used in all response objects
 global head
 head = {'content-type': 'application/json'}
+
 
 #Create Topic method
 @route('/topics', method='POST')
@@ -61,46 +66,96 @@ def createTopic():
 	else:
 		abort(400, 'Error: no body in the request')
 
-"""
-	New Comment
-"""
 
 #Delete Topic method
 @route('/topics/<topicId>', method='DELETE')
 def deleteTopic(topicId):
 	try:
-            print 'in try'
+            #print 'in try'
 	    topics.remove({'_id':ObjectId(topicId)})
 	except:
                abort(500, 'Error: database error')
 	body = {"topicId": 'Removed Successfully'}
 	return bottle.HTTPResponse(status=200, body=json.dumps(body), headers=head) 
 
+
 #Add comments to topic
 #POST method sample , add check to see if that topic_id exists
-
-
 @route('/topics/<topicId>/comments', method='POST')
 def addComments(topicId=None):
-    if not topicId:
-        abort(400, "Bad Request: Missing Argument")
+
+    if topicId is None:
+        abort(400, "Bad Request: missing argument")
+    else:
+	topic = db['topics'].find({"_id": ObjectId(str(topicId))})
+	if topic.count() < 1:
+		abort(400, "Failed: %s not found" %(topicId))
+
     if request.body.read():
            data = json.loads(request.body.read())
        	   if data:
-                   print "in if data"
+                   #print "in if data"
                    author = data['author']
                    author_comment = data['comment']
 		   try:
-		       result = topics.update({"_id":ObjectId(topicId)},{"$push":{"Comments":{"author":author,"comment":author_comment}}},upsert=FALSE)
-                       print result       
+		       result = db['topics'].update({"_id":ObjectId(str(topicId))},{"$push":{"comments":{"author":author,"comment":author_comment}}},upsert=False)   
                    except:
-                          print "\nin exception"     
+                          #print "\nin exception"     
                           abort(500, 'Error: database error')
                    return bottle.HTTPResponse(status=200, body=json.dumps(result), headers=head)
            else:
                 abort(400, 'Error: no data in body')
     else:
          abort(400, 'Error: no body in the request')
+
+
+#Get list of topics
+@route('/topics', method='GET')
+def get_topic():
+    """
+	This method is the API used for getting all topics on the reddit server.
+	The route is '/topics' and the HTTP method used is GET. The data is attached 
+    in the HTTP Request body as JSON. If the topics exists in storage, the list
+    of topics are returned to the client.
+    """
+    try:
+        all_topic = []
+	topics = db['topics'].find()
+	for topic in topics:
+		all_topic.append(topic)
+	print len(all_topic)
+#        for each_topic in db['topics'].find():
+#            topic = {}
+#            topic["topicName"]=each_topic.items()[0][0]
+#            topic["author"]=each_topic.items()[0][1]['author']
+#            topic["topicId"]=str(each_topic.items()[1][1])
+#            all_topic.append(topic)
+        #print all_topic
+    except:
+        abort(500, 'Error: database error')
+    return bottle.HTTPResponse(status=200, body=json.dumps(str(all_topic)), headers=head)
+
+
+#Get topic by ID
+@route('/topics/<topicId>', method='GET')
+def get_topic_by_id(topicId=None):
+
+	if topicId is None:
+		abort(400, "Bad request: missing argument")
+	else:
+
+		try:
+			topic = db['topics'].find_one({"_id": ObjectId(str(topicId))})
+
+			try:
+				return bottle.HTTPResponse(status=200, body=json.dumps(str(topic)), headers=head)
+			except:
+				abort(400, "Failed: %s not found" %(topicId))
+
+		except Exception as e:
+			print e
+			abort(500, "Error: database error")
+
 
 #Start server
 run(host='localhost', port=8080, debug=True)
